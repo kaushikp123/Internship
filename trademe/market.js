3aa386b0fb83411247fce814f819ea65c03f472f
kaushik <kaushikprakash94@gmail.com>
2022-10-02 21:46:15 +0530

    internship projects

---
+++ trademe/market.js
@@ -0,0 +1,156 @@
+let tabButtons = document.querySelectorAll(".tab-container .button-container button");// this will hold all buttons
+        let tabPanel = document.querySelectorAll(".tab-container .tab-panel");//this will hold all panels
+        function showpanel(panelindex,color)
+        {
+          tabButtons.forEach(function(element)
+          {
+              element.style.backgroundColor="";
+          });
+          tabButtons[panelindex].style.backgroundColor = color;
+          tabPanel.forEach(function(element)
+          {
+              element.style.display="none";
+          });
+          tabPanel[panelindex].style.display="block";
+  
+        }
+        showpanel(0,'rgb(235, 244, 250)')
+        let stck = document.querySelectorAll(".stocks .stock");
+        function highlight(index,color)
+        {
+          stck.forEach(function(element){
+            element.style.backgroundColor="";
+          });
+          stck[index].style.backgroundColor=color;
+        }
+        highlight(0,'rgb(235, 244, 250)');
+        let tabButtons1 = document.querySelectorAll(".tab-container-1 .button-container-1 button");// this will hold all buttons
+        let tabPanel1 = document.querySelectorAll(".tab-container-1 .tab-panel-1");//this will hold all panels
+        function showpanel1(panelindex,color)
+        {
+          tabButtons1.forEach(function(element)
+          {
+              element.style.backgroundColor="";
+          });
+          tabButtons1[panelindex].style.backgroundColor = color;
+          tabPanel1.forEach(function(element)
+          {
+              element.style.display="none";
+          });
+          tabPanel1[panelindex].style.display="block";
+  
+        }
+        showpanel1(0,'rgb(235, 244, 250)')
+        let stck1 = document.querySelectorAll(".stocks-1 .stock-1");
+        function highlight1(index,color)
+        {
+          stck1.forEach(function(element){
+            element.style.backgroundColor="";
+          });
+          stck1[index].style.backgroundColor=color;
+        }
+        highlight1(0,'rgb(235, 244, 250)');
+        let tabButtons2 = document.querySelectorAll(".tab-container-2 .button-container-2 button");// this will hold all buttons
+        let tabPanel2 = document.querySelectorAll(".tab-container-2 .tab-panel-2");//this will hold all panels
+        function showpanel2(panelindex,color)
+        {
+          tabButtons2.forEach(function(element)
+          {
+              element.style.backgroundColor="";
+          });
+          tabButtons2[panelindex].style.backgroundColor = color;
+          tabPanel2.forEach(function(element)
+          {
+              element.style.display="none";
+          });
+          tabPanel2[panelindex].style.display="block";
+  
+        }
+        showpanel2(0,'rgb(235, 244, 250)')
+        let stck2 = document.querySelectorAll(".stocks-2 .stock-2");
+        function highlight2(index,color)
+        {
+          stck2.forEach(function(element){
+            element.style.backgroundColor="";
+          });
+          stck2[index].style.backgroundColor=color;
+        }
+        highlight2(0,'rgb(235, 244, 250)');
+        let tabButtons3 = document.querySelectorAll(".tab-container-3 .button-container-3 button");// this will hold all buttons
+        let tabPanel3 = document.querySelectorAll(".tab-container-3 .tab-panel-3");//this will hold all panels
+        function showpanel3(panelindex,color)
+        {
+          tabButtons3.forEach(function(element)
+          {
+              element.style.backgroundColor="";
+          });
+          tabButtons3[panelindex].style.backgroundColor = color;
+          tabPanel3.forEach(function(element)
+          {
+              element.style.display="none";
+          });
+          tabPanel3[panelindex].style.display="block";
+  
+        }
+        showpanel3(0,'rgb(235, 244, 250)')
+        let stck3 = document.querySelectorAll(".stocks-3 .stock-3");
+        function highlight3(index,color)
+        {
+          stck3.forEach(function(element){
+            element.style.backgroundColor="";
+          });
+          stck3[index].style.backgroundColor=color;
+        }
+        highlight3(0,'rgb(235, 244, 250)');
+        let tabButtons4 = document.querySelectorAll(".tab-container-4 .button-container-4 button");// this will hold all buttons
+        let tabPanel4 = document.querySelectorAll(".tab-container-4 .tab-panel-4");//this will hold all panels
+        function showpanel4(panelindex,color)
+        {
+          tabButtons4.forEach(function(element)
+          {
+              element.style.backgroundColor="";
+          });
+          tabButtons4[panelindex].style.backgroundColor = color;
+          tabPanel4.forEach(function(element)
+          {
+              element.style.display="none";
+          });
+          tabPanel4[panelindex].style.display="block";
+  
+        }
+        showpanel4(0,'rgb(235, 244, 250)')
+        let stck4 = document.querySelectorAll(".stocks-4 .stock-4");
+        function highlight4(index,color)
+        {
+          stck4.forEach(function(element){
+            element.style.backgroundColor="";
+          });
+          stck4[index].style.backgroundColor=color;
+        }
+        highlight4(0,'rgb(235, 244, 250)');
+        let tabButtons5 = document.querySelectorAll(".tab-container-5 .button-container-5 button");// this will hold all buttons
+        let tabPanel5 = document.querySelectorAll(".tab-container-5 .tab-panel-5");//this will hold all panels
+        function showpanel5(panelindex,color)
+        {
+          tabButtons5.forEach(function(element)
+          {
+              element.style.backgroundColor="";
+          });
+          tabButtons5[panelindex].style.backgroundColor = color;
+          tabPanel5.forEach(function(element)
+          {
+              element.style.display="none";
+          });
+          tabPanel5[panelindex].style.display="block";
+  
+        }
+        showpanel5(0,'rgb(235, 244, 250)')
+        let stck5 = document.querySelectorAll(".stocks-5 .stock-5");
+        function highlight5(index,color)
+        {
+          stck5.forEach(function(element){
+            element.style.backgroundColor="";
+          });
+          stck5[index].style.backgroundColor=color;
+        }
+        highlight5(0,'rgb(235, 244, 250)');
